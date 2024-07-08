15437b4b6d399b90f9e6f9776abef6c7b9d8e080
perezantolin986 <perezantolin986@gmail.com>
2024-07-08 14:23:27 +0000

    Finish Edition

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -2,30 +2,30 @@ export function trimText(input: string, maxLength: number = 100): string {
   if (input.length <= maxLength) return input;
   return input.substring(0, maxLength - 3) + "...";
 }
-export function getCurrentTimeInIndia(): Date {
+export function getCurrentTimeInSpain(): Date {
   // Create a date object with the current UTC time
   const now = new Date();
 
-  // Convert the UTC time to India's time
-  const offsetIndia = 5.5; // India is in UTC+5:30, but you might need to adjust this based on Daylight Saving Time.
-  now.setHours(now.getUTCHours() + offsetIndia);
+  // Convert the UTC time to Spain's time
+  const offsetSpain = 2; // Spain is in UTC+5:30, but you might need to adjust this based on Daylight Saving Time.
+  now.setHours(now.getUTCHours() + offsetSpain);
 
   return now;
 }
 
-export function formatTimeForIndia(date: Date): string {
+export function formatTimeForSpain(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     hour: "numeric",
     minute: "2-digit",
     second: "2-digit",
     hour12: true, // This will format the time in 12-hour format with AM/PM
-    timeZone: "Asia/Kolkata",
+    timeZone: "Europe/Madrid",
   };
 
   let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
 
-  // Indian Standard Time is abbereviated as "IST".
-  formattedTime += " IST";
+  // Spain Standard Time is abbereviated as "IST".
+  formattedTime += "";
 
   return formattedTime;
 }
